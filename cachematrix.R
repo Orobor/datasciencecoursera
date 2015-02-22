b12eaef3f4ec8be9fa7e260d7660c16412167417
Orobor Anderson Ise <get2ise@gmail.com>
2015-02-22 22:11:55 +0100

    rmatrix commit

---
+++ cachematrix.R
@@ -0,0 +1,26 @@
+## The function caching the inverse of a special matrix
+makeCacheMatrix <- function(x = matrix()) {
+i <- NULL
+set <- function(y) {
+x <<- y
+i <<- NULL
+}
+get <- function() x
+setinverse <- function(inverse) i <<- inverse
+getinverse <- function() i
+list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+
+## The function calculate the inverse of a special matrix and set it inverse using setinverse function
+cacheSolve <- function(x, ...) {
+i <- x$getinverse()
+if(!is.null(i)) {
+message("getting cached data")
+return(i)
+}
+data <- x$get()
+i <- solve(data, ...)
+x$setinverse(i)
+i
+}
